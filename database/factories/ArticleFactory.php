e999f267718b8d85e7a7e94c09c16e042b344bd7
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-09-03 22:17:36 +0300

    Add UserSeeder, change ArticleSeeder

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,15 +5,21 @@
 use App\Article;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Article::class, function (Faker $faker) {
+    $title = $faker->sentence(6);
+
     return [
         'category_id' => random_int(1, 3),
-        'title' => $faker->sentence(),
-        'slug' => $faker->unique()->safeEmail,
+        'title' => $title,
+        'slug' => Str::slug($title),
         'description' => $faker->paragraph(1),
         'content' => $faker->text(600),
-        'published_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'published_at' => Carbon::now()
+            ->subDays(random_int(1, 7))
+            ->subHours(random_int(1, 10))
+            ->format('Y-m-d H:i:s'),
         'is_published' => random_int(0, 1),
     ];
 });

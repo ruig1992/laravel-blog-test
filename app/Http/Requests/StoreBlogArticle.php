7a69d2822668881a9b55d5d5e8562cacaff95fdf
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-09-05 00:41:24 +0300

    Change unique rule

--- app/Http/Requests/StoreBlogArticle.php
+++ app/Http/Requests/StoreBlogArticle.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreBlogArticle extends FormRequest
 {
@@ -25,7 +26,7 @@ class StoreBlogArticle extends FormRequest
     {
         return [
             'category_id' => 'required|exists:categories,id',
-            'title' => 'required|unique:articles|max:255',
+            'title' => "required|unique:articles,title,{($this->article->id ?? 0)}",
             'description' => 'required|max:255',
             'content' => 'required',
             'published_at' => 'nullable|date',

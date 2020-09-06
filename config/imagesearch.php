3f755bb217deeed07a049ade39fc1c8a1098d666
Ruslan Danylevskyi <ruigdan9240@gmail.com>
2020-09-06 23:40:32 +0300

    Move to env for demo testing

--- config/imagesearch.php
+++ config/imagesearch.php
@@ -16,12 +16,12 @@ return [
 
     'google' => [
         // For creation Search Engine ID you have to go to https://cse.google.com/cse
-        'engine_id' => env('IMAGE_SEARCH_GOOGLE_ENGINE_ID', '91d967948853bc9b8'),
+        'engine_id' => env('IMAGE_SEARCH_GOOGLE_ENGINE_ID'),
 
         // For generation API key you have to go to https://console.developers.google.com
         // Max quota for free usage - 100 requests per 1 day
-        'api_key' => env('IMAGE_SEARCH_GOOGLE_API_KEY', 'AIzaSyA6za0pH0aUBsv0iTv65_CBJVRf6STX_J0'),
+        'api_key' => env('IMAGE_SEARCH_GOOGLE_API_KEY'),
 
-        'base_url' => env('IMAGE_SEARCH_GOOGLE_BASE_URL', 'https://customsearch.googleapis.com/customsearch/v1?searchType=image'),
+        'base_url' => env('IMAGE_SEARCH_GOOGLE_BASE_URL'),
     ],
 ];
